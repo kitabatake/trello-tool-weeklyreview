e7aa81ebed273ca55eed8d95e7570b066ef392ce
Tatsuya Kitabatake <e2tod2@gmail.com>
2020-01-28 15:29:15 +0900

    json unmarshal example

--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
+
+	//"time"
 )
 
-func main () {
-	//fmt.Println("yeah!" + os.Getenv("TRELLO_API_KEY"))
+type TrelloCard struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	DateLastActivity time.Time `json:"dateLastActivity"`
+}
 
+func main () {
 	values := url.Values{}
 	values.Add("key", os.Getenv("TRELLO_API_KEY"))
 	values.Add("token", os.Getenv("TRELLO_TOKEN"))
 	values.Add("fields", "name,dateLastActivity")
 	values.Add("filter", "closed")
-	values.Add("since", "2020-01-20")
+	values.Add("since", "2020-01-28")
 
 	endpoint := "https://api.trello.com/1/boards/qoy6rgiP/cards"
 	resp, err := http.Get(endpoint + "?" + values.Encode())
@@ -30,4 +38,12 @@ func main () {
 	fmt.Println("status :" + resp.Status)
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
+
+	var cards []TrelloCard
+	err = json.Unmarshal(body, &cards)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cards)
 }
